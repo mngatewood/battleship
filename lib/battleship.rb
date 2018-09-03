d2d52401a6dcab1f06769626b7a54d6b501d145a
Michael Gatewood <michael@mngatewood.com>
2018-09-02 23:32:15 -0600

    Adds initialize computer player methods and tests to battleship

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,8 +1,20 @@
+require './lib/game'
+require 'pry'
+
 class Battleship
 
-  def print_game_start_prompt
-    prompt =  "Welcome to BATTLESHIP \n \n Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-    print prompt
+  attr_reader :game
+
+  def initialize
+    @game = ""
+  end
+
+  def initialize_computer_player
+    game = Game.new
+    @game = game
+    game.create_computer_board
+    game.place_computer_ship("small_ship", 2)
+    game.place_computer_ship("large_ship", 3)
   end
 
 end
